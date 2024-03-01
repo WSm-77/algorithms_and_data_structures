814de92446903421d5e1b14196200ff407e79adf
wiktor <wiktor.sedzimir@gmail.com>
2024-03-01 17:41:47 +0100

    Add selectionSort.py

---
+++ sorting/selectionSort.py
@@ -0,0 +1,20 @@
+from random import randint
+
+def selection_sort(tab: list) -> None:
+    n = len(tab)
+
+    for i in range(n - 1):
+        miniIndex = i
+        for j in range(i + 1, n):
+            if tab[j] < tab[miniIndex]:
+                miniIndex = j
+            #end if
+        #end for
+        tab[i], tab[miniIndex] = tab[miniIndex], tab[i]
+    #end for
+
+if __name__ == "__main__":
+    tab = [randint(1, 100) for _ in range(8, 15)]
+    print(f"unsorted array: {tab}")
+    selection_sort(tab)
+    print(f"sorted array: {tab}")
