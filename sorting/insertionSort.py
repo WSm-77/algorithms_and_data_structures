24ed4ae5c558008263de98e80814209736a4d8f8
wiktor <wiktor.sedzimir@gmail.com>
2024-03-01 17:36:30 +0100

    Add insertionSort.py

---
+++ sorting/insertionSort.py
@@ -0,0 +1,22 @@
+from random import randint
+
+def insertion_sort(tab: list) -> None:
+    n = len(tab)
+
+    for i in range(2, n):
+        currenValue = tab[i]
+        tab[0] = currenValue
+        j = i - 1
+        while currenValue < tab[j]:
+            tab[j+1] = tab[j]
+            j -= 1
+        #end while
+        tab[j+1] = currenValue
+    #end for
+
+
+if __name__ == "__main__":
+    tab = [randint(1, 100) for _ in range(8, 15)]
+    print(f"unsorted array: {tab[1:]}")
+    insertion_sort(tab)
+    print(f"sorted array: {tab[1:]}")
