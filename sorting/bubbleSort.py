71a2ecea9c89a378a247ddd3fafa998e11499dbb
wiktor <wiktor.sedzimir@gmail.com>
2024-03-02 17:41:58 +0100

    Add bubbleSort.py

---
+++ sorting/bubbleSort.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def bubble_sort(tab: list) -> None:
+    n = len(tab)
+
+    for i in range(n - 1, 0, -1):
+        for j in range(i):
+            if tab[j] > tab[j+1]:
+                tab[j], tab[j+1] = tab[j+1], tab[j]
+
+if __name__ == "__main__":
+    tab = [randint(1, 100) for _ in range(8, 15)]
+    print(f"unsorted array: {tab}")
+    bubble_sort(tab)
+    print(f"sorted array: {tab}")
