382467771dcc7fc6ff66e836cdac9ffded03205b
wiktor <wiktor.sedzimir@gmail.com>
2024-03-13 20:18:01 +0100

    Added description to working solution

--- offline/offline2/zad2.py
+++ offline/offline2/zad2.py
@@ -1,32 +1,90 @@
+# Wiktor Sędzimir
+#
+# Opis:
+# Algorytm opiera się na stworzeniu posortowanej tablicy z "p" pierwszymi wartościami wejściowej tablicy. Dzięki temu łatwo możemy odczytać
+# wartość k-tego największego elementu z danego przedziału. W kolejnych krokach dodajemy następne elementy z wejściowej tablic do 
+# posortowanej tablicy pomocniczej utrzymując w niej porządek rosnoący, aby móc łatwo odczytywać k-ty największy element i dodać jego 
+# wartość do porządanej sumy.   
+
 from zad2testy import runtests
 
-def partition(tab, beg, end):
-    pivot = end
-    i = beg
-    for j in range(beg, end):
-        if tab[j] <= tab[pivot]:
-            tab[j], tab[i] = tab[i], tab[j]
-            i += 1
+def binary_search(tab, val):
+    n = len(tab)
+    end = n - 1
+    beg = 0
+    while beg <= end:
+        mid = (beg + end) // 2
+        if val < tab[mid]:
+            end = mid - 1
+        elif val > tab[mid]:
+            beg = mid + 1
+        else:
+            return mid
     
-    tab[i], tab[pivot] = tab[pivot], tab[i]
-    return i
+    return beg
 
-def quick_select(tab, beg, end, k):
+def binary_insert(tab, val):
+    beg, end = 0, len(tab) - 1
+    while beg <= end:
+        mid = (beg + end) // 2
+        if val >= tab[mid]:
+            beg = mid + 1
+        else:
+            end = mid - 1
+    return beg
+
+
+def quick_sort(tab, beg, end):
+    if beg >= end:
+        return
+    
     pivot = partition(tab, beg, end)
-    if pivot == k:
-        return tab[pivot]
-    elif k < pivot:
-        return quick_select(tab, beg, pivot - 1, k)
-    else:
-        return quick_select(tab, pivot + 1, end, k)
+    quick_sort(tab, beg, pivot - 1)
+    quick_sort(tab, pivot + 1, end)
+
+def partition(tab, beg, end):
+    pivot = (beg + end) // 2
+    if tab[beg] > tab[pivot]:
+        tab[beg], tab[pivot] = tab[pivot], tab[beg]
+    
+    if tab[pivot] < tab[end]:
+        tab[pivot], tab[end] = tab[end], tab[pivot]
+
+    pivot = end
+    end -= 1
+
+    while beg <= end:
+        if tab[beg] <= tab[pivot]:
+            beg += 1
+        elif tab[end] >= tab[pivot]:
+            end -= 1
+        else:
+            tab[beg], tab[end] = tab[end], tab[beg]
+            beg += 1
+            end -= 1
+        
+    tab[beg], tab[pivot] = tab[pivot], tab[beg]
+    return beg
 
 def ksum(T, k, p):
     # tu prosze wpisac wlasna implementacje
     n = len(T)
-    result = 0
-    for i in range(n - p + 1):
-        helpArray = [T[j] for j in range(i, p+i)]
-        result += quick_select(helpArray, 0, p - 1, p - k)
+    helpArray = [T[j] for j in range(p)]
+    quick_sort(helpArray, 0, p - 1)
+    result = helpArray[-k]
+
+    for i in range(1, n - p + 1):
+        valueToRemove = T[i-1]
+        valueToInsert = T[i+p-1]
+
+        toRemoveIndex = binary_search(helpArray, valueToRemove)
+        del helpArray[toRemoveIndex]
+
+        toInsertIndex = binary_insert(helpArray, valueToInsert)
+        helpArray.insert(toInsertIndex, valueToInsert)
+
+        result += helpArray[-k]
+        
     return result
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
