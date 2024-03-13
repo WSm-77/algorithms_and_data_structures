f61e90df68c985e388702200c156ea7431902932
wiktor <wiktor.sedzimir@gmail.com>
2024-03-13 12:31:08 +0100

    Working zad2.py

--- offline/offline2/zad2.py
+++ offline/offline2/zad2.py
@@ -1,5 +1,32 @@
 from zad2testy import runtests
 
+def binary_search(tab, val):
+    n = len(tab)
+    end = n - 1
+    beg = 0
+    while beg <= end:
+        mid = (beg + end) // 2
+        # print("bin s", beg, mid, end)
+        if val < tab[mid]:
+            end = mid - 1
+        elif val > tab[mid]:
+            beg = mid + 1
+        else:
+            return mid
+    
+    return beg
+
+def binary_insert(tab, val):
+    beg, end = 0, len(tab) - 1
+    while beg <= end:
+        mid = (beg + end) // 2
+        if val >= tab[mid]:
+            beg = mid + 1
+        else:
+            end = mid - 1
+    return beg
+
+
 def quick_sort(tab, beg, end):
     if beg >= end:
         return
@@ -35,22 +62,23 @@ def partition(tab, beg, end):
 def ksum(T, k, p):
     # tu prosze wpisac wlasna implementacje
     n = len(T)
-    result = 0
+    helpArray = [T[j] for j in range(p)]
+    quick_sort(helpArray, 0, p - 1)
+    result = helpArray[-k]
 
-    for i in range(n - p + 1):
-        helpArray = [T[j] for j in range(i, i + p)]
-        quick_sort(helpArray, 0, p - 1)
-        # print(helpArray)
+    for i in range(1, n - p + 1):
+        valueToRemove = T[i-1]
+        valueToInsert = T[i+p-1]
+
+        toRemoveIndex = binary_search(helpArray, valueToRemove)
+        del helpArray[toRemoveIndex]
+
+        toInsertIndex = binary_insert(helpArray, valueToInsert)
+        helpArray.insert(toInsertIndex, valueToInsert)
+
         result += helpArray[-k]
         
     return result
 
-
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( ksum, all_tests=True )
-
-
-# tab = [7,9,1,5,8,6,2,12]
-# k = 4
-# p = 5
-# print(ksum(tab, k, p))
