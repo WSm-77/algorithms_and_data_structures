3ee6badb3d0e3e63c727af027fad0785fdf0174c
wiktor <wiktor.sedzimir@gmail.com>
2024-03-21 10:23:23 +0100

    Added itterative heapify

--- sorting/heapSort.py
+++ sorting/heapSort.py
@@ -1,31 +1,51 @@
 from random import randint
 
-def heapify(heap, heapLen, index):
+def heapify_rek(heap, heapLen, index):
     maxi = index
     left = 2*index + 1
     right = 2*index + 2
 
-    if left < heapLen and tab[left] > tab[maxi]:
+    if left < heapLen and heap[left] > heap[maxi]:
         maxi = left
 
-    if right < heapLen and tab[right] > tab[maxi]:
+    if right < heapLen and heap[right] > heap[maxi]:
         maxi = right
 
     if maxi != index:
-        tab[maxi], tab[index] = tab[index], tab[maxi]
-        heapify(heap, heapLen, maxi)
+        heap[maxi], heap[index] = heap[index], heap[maxi]
+        heapify_rek(heap, heapLen, maxi)
+
+def heapify_it(heap, heapLen, toFix):
+    while True:
+        maxi = toFix
+        left = 2*toFix + 1
+        right = 2*toFix + 2
+
+        if left < heapLen and heap[left] > heap[maxi]:
+            maxi = left
+
+        if right < heapLen and heap[right] > heap[maxi]:
+            maxi = right
+
+        if maxi != toFix:
+            heap[maxi], heap[toFix] = heap[toFix], heap[maxi]
+            toFix = maxi
+        else:
+            break
+    #end while
+        
 
 def build_heap(tab):
     n = len(tab)
     for i in range(n - 1, -1, -1):
-        heapify(tab, n, i)
+        heapify_it(tab, n, i)
 
 def heap_sort(tab):
     n = len(tab)
     build_heap(tab)
     for i in range(n - 1, -1 , -1):
         tab[0], tab[i] = tab[i], tab[0]
-        heapify(tab, i, 0)
+        heapify_it(tab, i, 0)
 
 
 if __name__ == "__main__":
