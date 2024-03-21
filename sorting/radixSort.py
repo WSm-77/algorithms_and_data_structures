f2a9c974cbf5f14b5895475407e2d15be16e6fff
wiktor <wiktor.sedzimir@gmail.com>
2024-03-21 19:53:25 +0100

    Add radixSort.py

---
+++ sorting/radixSort.py
@@ -0,0 +1,39 @@
+def char2index(char):
+    return ord(char) - ord("a")
+
+def radix_sort(words: list[str]):
+    n = len(words)
+    wordLen = len(words[0])
+
+    for letterIndex in range(wordLen - 1, -1, -1):
+
+        # we consider only latin alphabet
+        letterCnt = [0]*26
+        sortedWords = [None]*n
+
+        for wordIndex in range(n):
+            letterCnt[char2index(words[wordIndex][letterIndex])] += 1
+        #end for
+        for i in range(1, 26):
+            letterCnt[i] += letterCnt[i - 1]
+        #end for
+        for wordIndex in range(n - 1, -1, -1):
+            currentWord = words[wordIndex]
+            charIndex = char2index(currentWord[letterIndex])
+            letterCnt[charIndex] -= 1
+            sortedWords[letterCnt[charIndex]] = currentWord
+        #end for
+
+        words = sortedWords
+    #end for
+            
+    return words
+
+
+
+
+if __name__ == "__main__":
+    testWords = ["kra", "art", "kot", "kit", "ati", "kil", "ala", "kac", "koc", "kis", "aka"]
+    print(testWords)
+    testWords = radix_sort(testWords)
+    print(testWords)
