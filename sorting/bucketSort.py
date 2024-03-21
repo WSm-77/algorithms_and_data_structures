0b42b380ab96ac807ce768400b142f2108515e6b
wiktor <wiktor.sedzimir@gmail.com>
2024-03-21 11:11:23 +0100

    Improved bucketSort.py

--- sorting/bucketSort.py
+++ sorting/bucketSort.py
@@ -12,21 +12,23 @@ def insertion_sort(tab, tabLen):
         tab[j] = tab[0]
     #end for
 
-# sorting numbers < 0, 1 )
-def bucket_index(ele, n):
-    return floor(n*ele)
+# sorting numbers < intervalStart, intervalEnd )
+def bucket_index(val, tabLen, intervalStart, intervalEnd):
+    # return floor(val*tabLen)
+    return floor((val - intervalStart)*tabLen / (intervalEnd - intervalStart))
 
-def bucket_sort(tab):
+def bucket_sort(tab, intervalStart, intervalEnd):
     n = len(tab)
 
     sortedTab = [0]*n
     sortedIndex = 0
 
-    # i-th bucket represents interval from < i/n, (i+1)/n ) 
+    # i-th bucket represents interval from < intervalStart + (i*intervalSize)/n, intervalStart + ((i+1)*(intervalSize))/n ) 
     buckets: list[list[float]] = [[0.0] for _ in range(n)]
 
     for ele in tab:
-        buckets[bucket_index(ele, n)].append(ele)
+        # buckets[bucket_index(ele, n)].append(ele)
+        buckets[bucket_index(ele, n, intervalStart, intervalEnd)].append(ele)
     #end for
     
     for bucket in buckets:
@@ -41,10 +43,13 @@ def bucket_sort(tab):
             
 if __name__ == "__main__":
     n = 20
-    testTab = [randint(0, n - 1) / n for _ in range(n)]
+    intervalStart = 2
+    intervalEnd = 4
+    precision = 1000
+    testTab = [randint(intervalStart*precision, intervalEnd*(precision - 1)) / (precision) for _ in range(n)]
     print("unsorted:")
     print(testTab, end='\n\n')
-    
-    testTab = bucket_sort(testTab)
+
+    testTab = bucket_sort(testTab, intervalStart, intervalEnd)
     print("sorted:")
     print(testTab)
