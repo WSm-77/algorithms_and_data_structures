bc836a2fe6166b512b91aa49a5494d0c617728f1
wiktor <wiktor.sedzimir@gmail.com>
2024-04-20 17:58:27 +0200

    Full solution for zad4.py

--- offline/offline4/zad4.py
+++ offline/offline4/zad4.py
@@ -1,3 +1,13 @@
+# Wiktor Sędzimir
+# 
+# Opis algorytmu:
+# Najpierw zamieniamy tablicę krawędzi na graf reprezentowany w postaci tablicy sąsiedztwa. Następnie dla każdej krawędzi
+# wychodzącej z wierzchołka startowego uruchamiamy dfs, który dodatkowo pilnuje, żeby samolot leciał na stałej wysokości.
+# Wybranie pierwszej krawędzi dla naszego samolotu determinuje nam zakres, w której ostateczna wysokość przelotu musi się 
+# zmieścić. Przy przechodzeniu przez kolejne krawędzie grafu zakres wysokości nam się zawęża w zależności od dopuszczalnej
+# wysokości przelotu. Jeżeli dla pewnej krawędzi wychodzącej z wierzchołka startowego znajdziemy trasę pomiędzy x a y to 
+# kończymy wykonywanie algorytmu.
+
 from zad4testy import runtests
 
 def get_number_of_verticies(edgesTab):
@@ -7,20 +17,15 @@ def get_number_of_verticies(edgesTab):
   #end for
   return V + 1
 
-def graph_to_matrix(edgesTab, V):
-  matrix = [[-1 for _ in range(V)] for _ in range(V)]
+def edges_to_graph(edgesTab, V):
+  graph = [[] for _ in range(V)]
   
   for v, u, ceiling in edgesTab:
-    matrix[v][u] = matrix[u][v] = ceiling
+    graph[v].append((u, ceiling))
+    graph[u].append((v, ceiling))
   #end for
 
-  return matrix
-
-def fix_matrix(matrix):
-  n = len(matrix)
-  for i in range(n):
-    for j in range(n):
-      matrix[i][j] = abs(matrix[i][j])
+  return graph
 
 def does_intersect(interval1beg, interval1end, interval2beg, interval2end):
   return interval1beg <= interval2beg <= interval1end \
@@ -32,49 +37,41 @@ def Flight(L,x,y,t):
   # tu prosze wpisac wlasna implementacje
 
   def dfs_visit(vertex, minCeiling, maxCeiling):
-    nonlocal G, V, y, t, way
+    nonlocal G, V, y, t, way, visited
     if vertex == y:
-      nonlocal minCeilingMain, maxCeilingMain
-      minCeilingMain = minCeiling
-      maxCeilingMain = maxCeiling
       return True
     
-    for neighbour in range(V):
-      currentCeiling = G[vertex][neighbour]
-      if 0 <= currentCeiling:
+    visited[vertex] = True
+    
+    for neighbour, currentCeiling in G[vertex]:
+      if not visited[neighbour]:
         if does_intersect( minCeiling, maxCeiling, currentCeiling - t, currentCeiling + t):
-          way.append((neighbour, G[vertex][neighbour]))
           newMinCeiling = max(minCeiling, currentCeiling - t)
           newMaxCeiling = min(maxCeiling, currentCeiling + t)
-          
-          G[vertex][neighbour] = G[neighbour][vertex] = -currentCeiling
-
 
           if dfs_visit(neighbour, newMinCeiling, newMaxCeiling):
             return True
-          
-          way.pop()
     #end if
 
     return False
   #end def
 
   V = get_number_of_verticies(L)
-  G = graph_to_matrix(L, V)
+  G = edges_to_graph(L, V)
+
   way = [x]
 
-  for neighbour in range(V):
-    if G[x][neighbour] > 0:
-      minCeilingMain = G[x][neighbour] - t
-      maxCeilingMain = G[x][neighbour] + t
-      way.append((neighbour, G[x][neighbour]))
-      if dfs_visit(neighbour, minCeilingMain, maxCeilingMain):
-        print(f"ceiling: {minCeilingMain} ~ {maxCeilingMain}")
-        print(*way, sep=" -> ")
+  for neighbour, ceiling in G[x]:
+    visited = [False]*V
+    visited[x] = True
 
-        return True
-      #end if
-      way.pop()
+    minCeilingMain = ceiling - t
+    maxCeilingMain = ceiling + t
+
+    way.append((neighbour, ceiling))
+    if dfs_visit(neighbour, minCeilingMain, maxCeilingMain):
+      return True
+    #end if
   return False
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
