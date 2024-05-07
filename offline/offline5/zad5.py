13159eef03abf85dc52095b604747ffdf226dc42
wiktor <wiktor.sedzimir@gmail.com>
2024-05-07 15:52:56 +0200

    Solution with matrix reprezentation

--- offline/offline5/zad5.py
+++ offline/offline5/zad5.py
@@ -1,69 +1,66 @@
 # Wiktor Sędzimir
 # 
 # Opis algorytmu:
-# Ideą algorytmu jest zamiana grafu w postaci listy krawędzi na listę sąsiedztwa i następnie dodanie dodatkowych krawędzi łączących
+# Ideą algorytmu jest zamiana grafu w postaci listy krawędzi na macierz jednocześnie dodając nowe krawędzie łączące
 # planety znajdujące się koło osobliwości z kosztem przejścia przez taką krawędź wynoszącym 0. Następnie korzystamy z algorytmu 
-# Dijkstry do znalezienia najkrótszej ścieżki pomiędzy wierzchołkami a i b. Algorytm ma złożoność O( V^2 log V ), ponieważ dodanie
-# kolejnych krawędzi może spowodować powstanie grafu pełnego, w którym liczba krawędzi jest rzędu V^2, natomiast złożoność czasowa
-# algorymu Dijkstry jest rzędu E log V.
+# Dijkstry do znalezienia najkrótszej ścieżki pomiędzy wierzchołkami a i b. Algorytm ma złożoność O( V^2 ), ponieważ korzysta
+# z algorytmu Dijkstry dla reprezentacji macierzowej.
 
 from zad5testy import runtests
 from queue import PriorityQueue
 
-# dodaje krawędzie pomiędzy osobliwościami
-def edges_to_graph(E, V, S):
-    graph = [[] for _ in range(V)]
-
-    # graf pierwotny
-    for vertex, neighbour, cost in E:
-        graph[vertex].append((neighbour, cost))
-        graph[neighbour].append((vertex, cost))
-
+def edges_to_matrix(edges, V, S):
     nextToSingularity = [False]*V
+
     for vertex in S:
         nextToSingularity[vertex] = True
 
-    # graf z uwzględnieniem krawędzi pomiędzy osobliwościami
+    INF = float("inf")
+    matrix = [[INF for _ in range(V)] for _ in range(V)]
+
+    for vertex, neighbour, cost in edges:
+        matrix[vertex][neighbour] = cost
+        matrix[neighbour][vertex] = cost
+
     for vertex in range(V):
+        matrix[vertex][vertex] = 0
         if nextToSingularity[vertex]:
             for neighbour in S:
-                if neighbour == vertex:
-                    continue
+                matrix[vertex][neighbour] = 0
 
-                graph[vertex].append((neighbour, 0))
-            #end for
-    #end for
-
-    return graph
+    return matrix
 
 def spacetravel( n, E, S, a, b ):
     # tu prosze wpisac wlasna implementacje
 
     V = n
-
-    G = edges_to_graph(E, V, S)
-    distances = [float("inf")]*V
-    toVisit = PriorityQueue()
-    toVisit.put((0, a))
+    G = edges_to_matrix(E, V, S)
+    INF = float("inf")
+    distances = [INF]*V
     distances[a] = 0
+    relaxed = [False]*V
 
-    while not toVisit.empty():
-        distance, vertex = toVisit.get()
-        
-        if distance > distances[vertex]:
-            continue
-        elif vertex == b:
-            return distances[b]
+    for _ in range(V):
+        vertexToRelax = None
+        minDistance = INF
 
-        for neighbour, cost in G[vertex]:
-            if distance + cost < distances[neighbour]:
-                distances[neighbour] = distance + cost
-                toVisit.put((distances[neighbour], neighbour))
-            #end if
-        #end for
+        for vertex in range(V):
+            if not relaxed[vertex] and distances[vertex] < minDistance:
+                vertexToRelax = vertex
+                minDistance = distances[vertex]
+
+        if minDistance == INF:
+            break
+
+        relaxed[vertexToRelax] = True
+
+        for neighbour in range(V):
+            cost = G[vertexToRelax][neighbour]
+            if cost >= 0:
+                if distances[vertexToRelax] + cost < distances[neighbour]:
+                    distances[neighbour] = distances[vertexToRelax] + cost
 
     return distances[b] if distances[b] != float("inf") else None
-    # return None
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( spacetravel, all_tests = True )
