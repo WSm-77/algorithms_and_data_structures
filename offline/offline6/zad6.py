123a6fb6a47a1e4673deb0bcead7df4791cabd91
wiktor <wiktor.sedzimir@gmail.com>
2024-05-07 14:45:22 +0200

    Working zad6.py

--- offline/offline6/zad6.py
+++ offline/offline6/zad6.py
@@ -1,4 +1,15 @@
+# Wiktor Sędzimir
+# 
+# Opis algorytmu:
+# Dla każdego wierzchołka rozważamy minimalny dystans, który należy przebyć, aby znaleźć się w danym wierzchołku,
+# przy uwzględnieniu, czy do wirzchołka wchodzimy wykorzystując dwumilowe buty czy też nie (rozróżniamy to w tablicy
+# distances). Do kolejki priorytetowej dodajemy dodatkowe informacje o poprzednim koszcie podróży, dystansie poprzedniego
+# wierzchołka oraz czy do dotarcia do poprzedniego wierzchołka skorzystano z butów, po to aby móc rozważać kiedy warto
+# użyć dwumilowych butów. Algorytm ma złożoność O(V^2 log V), ponieważ korzysta z algorymu dijkstry dla macierzy, który
+# wykorzystuje kolejkę priorytetową
+
 from zad6testy import runtests
+from queue import PriorityQueue
 
 # definicja stałej globalnej
 INF = float("inf")
@@ -6,20 +17,20 @@ INF = float("inf")
 def find_not_visited_min_distance(distances, visited, V):
     minDistance = INF
     minVertex = None
-    usedBootsInPreviousJump = 0
+    bootsUsage = 0
 
     for vertex in range(V):
         if not visited[vertex][0] and distances[vertex][0] < minDistance:
             minDistance = distances[vertex][0]
             minVertex = vertex
-            usedBootsInPreviousJump = 0
+            bootsUsage = 0
 
         if not visited[vertex][1] and distances[vertex][1] < minDistance:
             minDistance = distances[vertex][1]
             minVertex = vertex
-            usedBootsInPreviousJump = 1
+            bootsUsage = 1
     
-    return minDistance, minVertex, usedBootsInPreviousJump
+    return minDistance, minVertex, bootsUsage
 
 def jumper( G, s, w ):
     # tu prosze wpisac wlasna implementacje
@@ -29,41 +40,31 @@ def jumper( G, s, w ):
     # distances[i][0] - dystans przebyty do i-tego wierzchołka bez używania dwumilowych butów w poprzednim skoku
     # distances[i][1] - dystans przebyty do i-tego wierzchołka z użyciem dwumilowych butów w poprzednim skoku
     distances = [[INF, INF] for _ in range(V)]
-    visited = [[False, False] for _ in range(V)]
-    distances[s][0] = 0
     distances[s][1] = 0
-    visited[s][1] = True
+    toCheck = PriorityQueue()
+    toCheck.put((0, s, 0, INF, INF, 1))
 
-    print(*G, sep = "\n")
+    while not toCheck.empty():
+        currentDistance, minVertex, bootsUsage, prevDistance, prevCost, prevBootsUsage = toCheck.get()
 
-    # wykonujemy tę pętlę 2*V - 1 razy, ponieważ musimy uzupełnić wszystkie elementy tablicy ditances z wyjątkiem distances[s][1]
-    for _ in range(2*V - 1):
-        currentDistance, minVertex, usedBootsInPreviousJump = find_not_visited_min_distance(distances, visited, V)
-
-        if currentDistance == INF:
-            break
-
-        visited[minVertex][usedBootsInPreviousJump] = True
+        if currentDistance < distances[minVertex][bootsUsage]:
+            distances[minVertex][bootsUsage] = currentDistance
 
         for neighbour in range(V):
             travelCost = G[minVertex][neighbour]
-
-            if travelCost > 0 and currentDistance + travelCost < distances[neighbour][0]:
-                distances[neighbour][0] = currentDistance + travelCost
+            if travelCost > 0:
+                newDistance = currentDistance + travelCost
+                if newDistance < distances[neighbour][0]:
+                    toCheck.put((newDistance, neighbour, 0, currentDistance, travelCost, bootsUsage))
             
-            # boots usage
-            if not usedBootsInPreviousJump:
-                for neighbourOfNeighbour in range(V):
-                    travelCost = max(travelCost, G[neighbour][neighbourOfNeighbour])
-
-                    if G[neighbour][neighbourOfNeighbour] > 0 and currentDistance + travelCost < distances[neighbourOfNeighbour][1]:
-                        distances[neighbourOfNeighbour][1] = currentDistance + travelCost
-    
-    print(distances)
-    print(visited)
+                if not bootsUsage and not prevBootsUsage:
+                    bootsTravelCost = max(travelCost, prevCost)
+                    bootsNewDistance = prevDistance + bootsTravelCost
+                    if bootsNewDistance < distances[neighbour][1]:
+                        toCheck.put((bootsNewDistance, neighbour, 1, INF, INF, 0))
 
 
     return min(distances[w][0], distances[w][1])
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( jumper, all_tests = False )
+runtests( jumper, all_tests = True )
