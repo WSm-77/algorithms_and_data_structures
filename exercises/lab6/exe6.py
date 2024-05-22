2ef72820d8a1cef9e240766cae7b080560b874c0
wiktor <wiktor.sedzimir@gmail.com>
2024-05-22 20:47:27 +0200

    Updated lab6/exe6.py

--- exercises/lab6/exe6.py
+++ exercises/lab6/exe6.py
@@ -1,17 +1,17 @@
 
 # Algocja leży na wielkiej pustyni i składa się z miast oraz oaz połączonych drogami. Każde miasto jest otoczone
 # murem i ma tylko dwie bramy północną i południową. Z każdej bramy prowadzi dokładnie jedna droga do jednej oazy
-# (ale do danej oazy może dochodzić dowolnie wiele dróg; oazy mogą też być połączone drogami między sobą). Prawo 
-# Algocji wymaga, że jeśli ktoś wjechał do miasta jedną bramą, to musi go opuścić drugą. Szach Algocji postanowił 
-# wysłać gońca, który w każdym mieście kraju odczyta zakaz formułowania zadań "o szachownicy" (obraza majestatu). 
-# Szach chce, żeby goniec odwiedził każde miasto dokładnie raz (ale nie ma ograniczeń na to ile razy odwiedzi każdą 
-# z oaz). Goniec wyjeżdża ze stolicji Algocji, miasta x, i po odwiedzeniu wszystkich miast ma do niej wrócić. Proszę 
+# (ale do danej oazy może dochodzić dowolnie wiele dróg; oazy mogą też być połączone drogami między sobą). Prawo
+# Algocji wymaga, że jeśli ktoś wjechał do miasta jedną bramą, to musi go opuścić drugą. Szach Algocji postanowił
+# wysłać gońca, który w każdym mieście kraju odczyta zakaz formułowania zadań "o szachownicy" (obraza majestatu).
+# Szach chce, żeby goniec odwiedził każde miasto dokładnie raz (ale nie ma ograniczeń na to ile razy odwiedzi każdą
+# z oaz). Goniec wyjeżdża ze stolicji Algocji, miasta x, i po odwiedzeniu wszystkich miast ma do niej wrócić. Proszę
 # przedstawić algorytm, który stwierdza czy odpowiednia trasa gońca istnieje.
 
 # Rozwiązanie:
 # Ponieważ miasta mają dokładnie dwa połączenia do dwóch oaz to możemy potraktować miasta jako krawędzie w naszym
 # grafie. Ponieważ pomiędzy oazami możemy przechodzić tyle razy ile chcemy to możemy złączyć skupiska połączonych oaz
-# w jedną oazę, która będzie naszym wierzhołkiem. Nasz problem sprowadza się wówczas do stwierdzenia czy dany graf 
+# w jedną oazę, która będzie naszym wierzhołkiem. Nasz problem sprowadza się wówczas do stwierdzenia czy dany graf
 # jest eulerowski.
 
 from collections import deque
@@ -66,9 +66,11 @@ def create_new_graph(oldGraph, newV, oldToNewVertexIdx):
     newGraph = [[] for _ in range(newV)]
 
     for oldVertex in range(oldV):
+
         # check if oldVertex an oasis, if not then we skip it for now
         if oldToNewVertexIdx[oldVertex] == None:
             continue
+
         newVertex = oldToNewVertexIdx[oldVertex]
 
         # print("\n###############")
@@ -114,17 +116,17 @@ def runner(edges, cities) -> bool:
 
 
 if __name__ == "__main__":
-    edges = [(0, 1), (1, 2), (2, 3), (3, 5), (4, 5), (4, 7), (5, 9), (3, 9), (1, 7), (0, 7), (7, 10), (10, 11),
-             (1, 6), (6, 8)]
+    graph28_edges = [(0, 1), (1, 2), (2, 3), (3, 5), (4, 5), (4, 7), (5, 9), (3, 9), (1, 7), (0, 7), (7, 10), (10, 11),
+                    (1, 6), (6, 8)]
     cities = [0, 2, 9, 4, 10, 6]
-    print(runner(edges, cities))
+    print(runner(graph28_edges, cities))
 
-    edges = edges + [(8, 12), (11, 12)]
+    edges = graph28_edges + [(8, 12), (11, 12)]
     cities = cities + [12]
     print(runner(edges, cities))
 
-    edges = [(0, 1), (1, 2), (0, 10), (2, 9), (2, 14), (2, 3), (3, 8), (8, 14), (9, 14), (10, 9), (3, 4),
-     (8, 7), (7, 4), (14, 7), (4, 5), (7, 6), (6, 5), (14, 6), (9, 12), (12, 14), (10, 11), 
-     (15, 6), (11, 17), (13, 17), (13, 16), (15, 16)]
+    graph29_edges = [(0, 1), (1, 2), (0, 10), (2, 9), (2, 14), (2, 3), (3, 8), (8, 14), (9, 14), (10, 9), (3, 4),
+                    (8, 7), (7, 4), (14, 7), (4, 5), (7, 6), (6, 5), (14, 6), (9, 12), (12, 14), (10, 11),
+                    (15, 6), (11, 17), (13, 17), (13, 16), (15, 16)]
     cities = [1, 11, 13, 12, 15, 5]
-    print(runner(edges, cities))
+    print(runner(graph29_edges, cities))
