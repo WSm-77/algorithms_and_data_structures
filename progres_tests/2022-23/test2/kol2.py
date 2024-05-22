a135a2fd0f7fb1b7c43ff89e4a0543f7fbecf5b1
wiktor <wiktor.sedzimir@gmail.com>
2024-05-22 21:36:53 +0200

    Optimized solution for 2022-23 test2 kol2.py

--- progres_tests/2022-23/test2/kol2.py
+++ progres_tests/2022-23/test2/kol2.py
@@ -1,5 +1,11 @@
 from kol2testy import runtests
 
+############
+# sposób I #
+############
+
+# Złożoność O(VE log* E)
+
 class FindUnion:
     def __init__(self, V):
         self.representants = [i for i in range(V)]
@@ -76,6 +82,81 @@ def beautree( G ):
         
     return None
         
+#############
+# sposób II #
+#############
 
+# Złożoność O(VE)
 
-runtests (beautree, all_tests = True)
+# Sortujemy krawędzie po najmniejszych wagach, a następnie tworzymy podgraf o V - 1 krawędziach z minimalnymi wagami
+# i sprawdzamy czy jest on drzewem: jeżeli tak, to znaleźliśmy minimalne piękne drzewo, w przeciwnym wypadku usuwamy 
+# z naszego podgrafu krawędź o najmniejszej wadze i dodajemy kolejną krawędź o najmniejszej wadze z oryginalnego grafu
+# i ponownie sprawdzamy czy jest on drzewem. Postępujemy tak do czasu znalezienia drzewa. Jeżeli nie znaleźliśmy żadnego
+# to oznacza to, że nie istnieje żadne piękne drzewo.
+
+def is_tree(treeCandidate):
+    def dfs(vertex):
+        nonlocal treeCandidate, visited, visitedCnt
+
+        visited[vertex] = True
+        visitedCnt += 1
+
+        for neighbour in treeCandidate[vertex]:
+            if not visited[neighbour]:
+                dfs(neighbour)
+    #end def
+
+    V = len(treeCandidate)
+    visited = [False for _ in range(V)]
+    visitedCnt = 0
+    
+    dfs(0)
+
+    return visitedCnt == V
+
+def beautree2( G ):
+    V = len(G)
+    edges = graph_to_edges(G)
+    E = len(edges)
+    edges.sort(key = lambda x: x[2])
+    treeSum = 0
+    tree = [[] for _ in range(V)]
+
+    # create graph from V - 1 least weighted edges
+    for edgeIdx in range(V - 1):
+        vertex, neighbour, weight = edges[edgeIdx]
+        tree[vertex].append(neighbour)
+        tree[neighbour].append(vertex)
+        treeSum += weight
+
+    if is_tree(tree):
+        return treeSum
+
+    for edgeIdx in range(E - V):
+        oldVertex, oldNeighbour, oldWeight = edges[edgeIdx]
+        oldNeighbourIdx = 0
+        oldVertexIdx = 0
+
+        while tree[oldVertex][oldNeighbourIdx] != oldNeighbour:
+            oldNeighbourIdx += 1
+        while tree[oldNeighbour][oldVertexIdx] != oldVertex:
+            oldVertexIdx += 1
+        
+        tree[oldVertex].pop(oldNeighbourIdx)
+        tree[oldNeighbour].pop(oldVertexIdx)
+
+        newVertex, newNeighbour, newWeight = edges[edgeIdx + V - 1]
+
+        tree[newVertex].append(newNeighbour)
+        tree[newNeighbour].append(newVertex)
+
+        treeSum = treeSum - oldWeight + newWeight
+
+        if is_tree(tree):
+            return treeSum
+    #end for
+
+    return None
+
+# runtests (beautree, all_tests = True)
+runtests (beautree2, all_tests = True)
